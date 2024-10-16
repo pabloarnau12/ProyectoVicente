4795b89580e90d5305d3a7e719cee63375b58411
PabloArnau <pablo.arnaulopez@cieep.com>
2024-10-16 12:32:57 +0200

    estilo de titulos

--- src/app/component/paginaPrincipal/bestsellers/bestsellers.component.ts
+++ src/app/component/paginaPrincipal/bestsellers/bestsellers.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 
 
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ApiService } from '../../../service/shop.service';
+import { calificacion } from '../../../common/Calificaciones';
+import { CalificacionesService } from '../../../service/calificaciones.service';
 
 @Component({
   selector: 'app-bestsellers',
@@ -12,12 +14,17 @@ import { ApiService } from '../../../service/shop.service';
   styleUrl: './bestsellers.component.css'
 })
 export class BestsellersComponent implements OnInit{
+  private readonly apiCalificaciones : CalificacionesService = inject(CalificacionesService)
   data : any = [];
   constructor(private router: Router, private apiService : ApiService) { }
+
+  califications: calificacion[] = [];
+  promedio: any;
 
   ngOnInit(): void {
 
     this.llenardata();
+    this.getCalificacionesbyID();
   }
 
 
@@ -28,5 +35,34 @@ export class BestsellersComponent implements OnInit{
       console.log(this.data);
       
     })
+  }
+
+  getCalificacionesbyID() {
+
+      this.apiCalificaciones.getCalificaciones().subscribe(
+        data => {
+          this.califications = data;
+          console.log(this.califications);
+          // this.calcularPromedio();
+        },
+        error => {
+          console.error('Error al obtener calificaciones:', error);
+        }
+      );
+  }
+  
+  calcularPromedio() {
+    if (this.califications && this.califications.length > 0) {
+      const suma = this.califications.reduce((acumulador, califications) => {
+        // Asumiendo que cada calificación tiene una propiedad 'valor'
+        return acumulador + califications.Calificacion_Establecimiento;
+      }, 0);
+      
+      this.promedio = suma / this.califications.length;
+      console.log('Promedio de calificaciones:', this.promedio);
+    } else {
+      console.log('No hay calificaciones para calcular el promedio.');
+      this.promedio = 0;
+    }
   }
 }
