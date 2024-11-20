ca6b8e6a035559d0bbd70efaa1c2fc4877960671
Pablo Arnau <pablo.arnaulopez@cieep.com>
2024-11-20 01:02:31 +0100

    eliminado los componentes d la pagina de contacto y añadido un nuevo estilo y organaizacion

--- src/app/component/Contact/contacto/contacto.component.ts
+++ src/app/component/Contact/contacto/contacto.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { ColumnasContactoComponent } from '../columnas-contacto/columnas-contacto.component';
-import { FormularioContactoComponent } from '../formulario-contacto/formulario-contacto.component';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [ColumnasContactoComponent, FormularioContactoComponent],
+  imports: [FormsModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
+  // Puedes agregar propiedades para almacenar los datos del formulario, si es necesario
+  nombre: string = '';
+  email: string = '';
+  mensaje: string = '';
 
+  // Método para enviar el formulario
+  onSubmit() {
+    console.log('Formulario enviado:', this.nombre, this.email, this.mensaje);
+    // Aquí puedes implementar la lógica para enviar los datos del formulario
+  }
 }
