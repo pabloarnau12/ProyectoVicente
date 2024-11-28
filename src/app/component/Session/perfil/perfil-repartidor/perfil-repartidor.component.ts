7896d555fe06e15dcb8525301821551bcf891628
Pablo Arnau <pablo.arnaulopez@cieep.com>
2024-11-28 23:05:47 +0100

    pipe fecha productos

--- src/app/component/Session/perfil/perfil-repartidor/perfil-repartidor.component.ts
+++ src/app/component/Session/perfil/perfil-repartidor/perfil-repartidor.component.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { ordersService } from '../../../../service/orders.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-perfil-repartidor',
@@ -18,7 +19,8 @@ import { ordersService } from '../../../../service/orders.service';
     MatMenuModule,
     MatButtonModule,
     MatCardModule,  
-    MatIconModule,  
+    MatIconModule,
+    DatePipe  
   ],
   templateUrl: './perfil-repartidor.component.html',
   styleUrl: './perfil-repartidor.component.css'
