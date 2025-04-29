42945f71df7c9396933549e9c68f15e3912bbaa4
Pablo Arnau <pablo.arnaulopez@cieep.com>
2025-04-29 18:29:18 +0200

    añadidas las interfaces de categorias services

---
+++ frontend/src/app/common/Categorias.ts
@@ -0,0 +1,5 @@
+export interface CategoriaEstablecimiento {
+  ID_Categoria: number; // ID único de la categoría
+  Nombre: string; // Nombre de la categoría
+  Descripcion?: string; // Descripción de la categoría (opcional)
+}
--- frontend/src/app/service/categorias.service.ts
+++ frontend/src/app/service/categorias.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { CategoriaEstablecimiento } from '../common/Categorias';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoriasService {
   private http: HttpClient = inject(HttpClient);
-  private urlApiCategorias = "http://localhost:3300/api/categorias/"
-  constructor() { }
+  private urlApiCategorias = 'http://localhost:3300/api/categorias/';
+  constructor() {}
 
-  public getCategoriasEstablecimientos(){
-    return this.http.get<any>(this.urlApiCategorias+'establecimientos');
+  public getCategoriasEstablecimientos() {
+    return this.http.get<CategoriaEstablecimiento[]>(
+      this.urlApiCategorias + 'establecimientos'
+    );
   }
 }
