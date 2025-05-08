3b9835ce0a89d0df997a3a443058cb8c10125139
Pablo Arnau <pablo.arnaulopez@cieep.com>
2025-05-08 20:04:05 +0200

    cambiados texto y precio promocion en el producto detail

--- frontend/src/app/component/paginaPrincipal/area-banner/area-banner.component.ts
+++ frontend/src/app/component/paginaPrincipal/area-banner/area-banner.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-area-banner',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './area-banner.component.html',
-  styleUrl: './area-banner.component.css'
+  styleUrl: './area-banner.component.css',
 })
-export class AreaBannerComponent {
-
-}
+export class AreaBannerComponent {}
